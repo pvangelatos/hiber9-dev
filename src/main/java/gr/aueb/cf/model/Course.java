63a3904a319a0fe4a22ba301e74dae67d02433b6
pvangelatos <p.vangelatos23@hotmail.com>
2026-01-21 17:46:41 +0200

    feat: add model entities

---
+++ src/main/java/gr/aueb/cf/model/Course.java
@@ -0,0 +1,51 @@
+package gr.aueb.cf.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "courses")
+public class Course {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String title;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "teacher_id")
+    private Teacher teacher;
+
+    public Course() {
+
+    }
+
+    public Course(String title) {
+        this.title = title;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Teacher getTeacher() {
+        return teacher;
+    }
+
+    public void setTeacher(Teacher teacher) {
+        this.teacher = teacher;
+    }
+}
---
+++ src/main/java/gr/aueb/cf/model/Teacher.java
@@ -0,0 +1,86 @@
+package gr.aueb.cf.model;
+
+import jakarta.persistence.*;
+
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "teachers")
+public class Teacher {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "firstname", length = 255, nullable = false, unique = false )
+    private String firstname;
+
+    @Column(nullable = false)
+    private String lastname;
+
+    @OneToMany(mappedBy = "teacher")
+    private Set<Course> courses = new HashSet<>();
+
+    public Teacher() {
+
+    }
+
+    public Teacher(String firstname, String lastname) {
+        this.firstname = firstname;
+        this.lastname = lastname;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    protected Set<Course> getCourses() {
+        return courses;
+    }
+
+    public Set<Course> getAllCourses() {
+        return Collections.unmodifiableSet(courses);
+    }
+
+    private void setCourses(Set<Course> courses) {
+        this.courses = courses;
+    }
+
+    public void addCourse(Course course) {
+        if (courses == null) courses = new HashSet<>();
+        courses.add(course);
+        course.setTeacher(this);
+    }
+
+    public void removeCourse (Course course) {
+        courses.remove(course);
+        course.setTeacher(null);
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%d %s %s",id, firstname, lastname);
+    }
+}
